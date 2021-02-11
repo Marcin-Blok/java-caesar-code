314b3ee1fbdeedc84d9242a140a6c24b77993dd4
Szymon Leyk <biuro@szymonleyk.pl>
2021-02-11 20:35:45 +0100

    #1 odkowanie tekstu

--- src/CaesarCode.java
+++ src/CaesarCode.java
@@ -8,5 +8,14 @@ public class CaesarCode {
         }
         return newString.toString();
     }
-    
+
+    public static String decode(String text){
+        StringBuilder newString = new StringBuilder();
+        for(char c : text.toCharArray()){
+            char encodedChar = (char)(65 + ( c - 42 ) % 26);
+            newString.append(encodedChar);
+        }
+        return newString.toString();
+    }
+
 }
--- src/CaesarCodeApp.java
+++ src/CaesarCodeApp.java
@@ -2,9 +2,18 @@ public class CaesarCodeApp {
     public static void main(String[] args) {
         String text = "ALAMAKOTAXYZ";
 
+        // wyświetlamy tekst
+        System.out.println(text);
+
         String encodedText = CaesarCode.encode(text);
 
+        // zakodowany tekst
         System.out.println(encodedText);
+
+        String decodedText = CaesarCode.decode(encodedText);
+
+        // odkodowany tekst
+        System.out.println(decodedText);
 
     }
 }
